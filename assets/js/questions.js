d94930691ef6e529b4294b94bb37286f4621a248
Lily Dell <lily.starleks@gmail.com>
2023-01-17 10:49:42 +0000

    Populated questions.js file with text to be displayed on the quiz page

---
+++ assets/js/questions.js
@@ -0,0 +1,32 @@
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed with:",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "Arrays in JavaScript can be used to store ______.",
+        choices: [
+            "numbers and strings",
+            "other arrays",
+            "booleans",
+            "all of the above"
+        ],
+        answer: "all of the above"
+    },
+    {
+        title: "String values must be enclosed within _____ when being assigned",
+        choices: ["commas", "curly brackets", "quotes", "parentheses"],
+        answer: "quotes"
+    },
+    {
+        title: "A very useful tool used during development and debbugging is _________",
+        choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+        answer: "console.log"
+    },
+];
